c3320a7ba7a807c628c8a8ad2c556315eb197573
Jiliang <liu_jiliang@qq.com>
2025-02-15 15:16:44 +0800

    第11章 准备部署

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -12,9 +12,9 @@ else:  # 否则使用四个斜线
     prefix = 'sqlite:////'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
-app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
